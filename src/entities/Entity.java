64e28bd206ec5177da517795a4bde764a6f6be5a
jdrumelhart <jdrumelhart@yahoo.com>
2015-09-24 10:36:58 -0700

    Wrote the walk method within Entity

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -3,6 +3,17 @@ package entities;
 public class Entity {
 	private int xPosition;
 	private int yPosition;
+	private enum Direction{
+		NORTH,
+		NORTHEAST,
+		EAST,
+		SOUTHEAST,
+		SOUTH,
+		SOUTHWEST,
+		WEST,
+		NORTHWEST
+	}
+	Direction direction;
 	
 	public Entity(int xPosition, int yPosition) {
 		this.xPosition = xPosition;
@@ -17,4 +28,40 @@ public class Entity {
 		return yPosition;
 	}
 
+	public void walk(Direction direction) {
+		switch(direction) {
+		case NORTH: 
+			yPosition --;
+			break;
+		case NORTHEAST:
+			yPosition --;
+			xPosition ++;
+			break;
+		case EAST:
+			xPosition ++;
+			break;
+		case SOUTHEAST:
+			yPosition ++;
+			xPosition ++;
+			break;
+		case SOUTH:
+			yPosition ++;
+			break;
+		case SOUTHWEST:
+			yPosition ++;
+			xPosition --;
+			break;
+		case WEST:
+			xPosition --;
+			break;
+		case NORTHWEST:
+			yPosition --;
+			xPosition --;
+			break;
+		default: 
+			System.out.println("Error: Invalid direction");
+			break;
+		
+		}
+	}
 }
