089f04bb1747d2cebc52352ed7794dbf1371e0c3
jdrumelhart <jdrumelhart@yahoo.com>
2015-09-28 11:48:31 -0700

    finished commenting in Game

--- src/window/Game.java
+++ src/window/Game.java
@@ -15,50 +15,44 @@ public class Game extends JFrame {
 	
 	//Default serialVersionUID
 	private static final long serialVersionUID = 1L;
-
-	private Player player;
 	
 	private ArrayList<Entity> entities;
 	
 	private ArrayList<Fence> boundingFences;
 	
 	private double freeEntities;
-
-	public boolean playerTurn;
 	
 	public Game() {
+		//ArrayList of entities on the board
 		entities = new ArrayList<Entity>();
 		
+		//ArrayList of the outer fences
 		boundingFences = new ArrayList<Fence>();
 		
 		setSize(500, 500);
 		
 		setBackground(Color.WHITE);
-	
-		player = new Player(10, 10);
 		
-		playerTurn = true;
-		
-		entities.add(player);
-		
+		//The number of entities still to be created
 		freeEntities = 33.0;
 		
 		//Creates the entities on the board randomly
 		for (int i = 0; i < 10 ; i++) {
 			for (int j = 0; j < 10 ; j++) {
 				double r = Math.random();
-				double threshold = (freeEntities /(100 - (i * 10) + j));
 				//Creates threshold for creation of entity
+				double threshold = (freeEntities /(100 - (i * 10) + j));
+				//Determines if random number is within threshold
 				if(r < threshold) {
+					//Creates new entity
 					entities.add(new Entity(i, j));
-					System.out.print("+");
 					freeEntities--;
 				}
-				else {
-					System.out.print("-");
-				}
 			}
-			System.out.println("");
+			//If finished creating entities, break
+			if(freeEntities <= 0) {
+				break;
+			}
 		}
 		
 		repaint();
