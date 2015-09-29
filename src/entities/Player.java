e571fd0bbb6cd19548bd6cfd44520742d37409d6
Eric Liu <ericxliu@gmail.com>
2015-09-29 11:10:30 -0700

    imported images into project, usable in code

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -1,14 +1,30 @@
 package entities;
 
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Player extends Entity {
 
 	public boolean playerTurn;
 	
+	BufferedImage sprite = null;
+	
 	public Player(int xPosition, int yPosition) {
 		super(xPosition, yPosition);
 		playerTurn = true;
+		
+		try{
+			sprite = ImageIO.read(new File("images/smily.jpg"));
+		} 
+		
+		catch(IOException e) {
+			
+		}
+		
 	}
 
 	@Override
