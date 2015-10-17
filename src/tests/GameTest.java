3b08088913002361796fdecf5d297f57b27fab0f
jdrumelhart <jdrumelhart@yahoo.com>
2015-10-17 16:26:58 -0700

    Wrote new test in GameTest

--- src/tests/GameTest.java
+++ src/tests/GameTest.java
@@ -4,14 +4,31 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import entities.Player;
 import window.Game;
 
 public class GameTest {
 
 	@Test
 	public void test() {
-		Game g1 = new Game();
-		
+		boolean hasPlayer = false;
+		int runLength = 500;
+		for(int m = 0; m < runLength; m++) {
+			Game g1 = new Game();
+			hasPlayer = false;
+			for(int i = 0; i < 11; i++) {
+				for(int j = 0; j < 11; j++) {
+					if(g1.getGrid()[i][j] instanceof Player) {
+						hasPlayer = true;
+					}
+				}
+			}
+			if(!(hasPlayer)) {
+				break;
+			}
+			
+		}
+		assertTrue("Does not have player", hasPlayer);
 	}
 
 }
