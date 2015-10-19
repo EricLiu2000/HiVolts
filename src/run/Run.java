73721d54db53ffec22ab77aa31ee3efe42622bf3
Eric Liu <ericxliu@gmail.com>
2015-10-18 19:03:43 -0700

    documented run

--- src/run/Run.java
+++ src/run/Run.java
@@ -7,6 +7,7 @@ import window.Game;
 //This class is used to run the program.
 public class Run {
 	/**
+	 * Calls runGame, which creates a new game.
 	 * 
 	 * Author: Eric Liu
 	 */
@@ -14,6 +15,11 @@ public class Run {
 		runGame();
 	}
 	
+	/**
+	 * This method creates a new game
+	 * 
+	 * Author: Eric Liu
+	 */
 	public static void runGame() {
 		Game game = new Game();
 		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
